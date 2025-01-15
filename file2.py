2663ff237036ce7b53fb3f50bc16db91362e1845
HayatGnd <hayatguendoul98@gmail.com>
2025-01-15 17:33:46 +0100

    file2 add

---
+++ file2.py
@@ -0,0 +1,20 @@
+x=1+3
+y=3*x+x**2
+print(y)
+hourly_wage = 20.0
+hours = 6
+day = "sunday"
+
+daily_wages = hourly_wage * hours
+print("condition:", day == "sunday")
+if day == "sunday":
+   print("wages before:", daily_wages)
+   daily_wages *= 2
+   print("wages after doubling:", daily_wages)
+
+print(f"Daily wages: {daily_wages} euros")
+
+degree= float(input("entrer degree :"))
+var2 = 5/9*(degree-32)
+print(var2)
+
