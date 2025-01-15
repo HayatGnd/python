3b49f1a389311c30ee718e943ed15d2ac4da6c85
HayatGnd <hayatguendoul98@gmail.com>
2025-01-15 22:58:07 +0100

    the older age

---
+++ exo4.py
@@ -0,0 +1,8 @@
+age1=int(input("entrer l'age de persone 1 : "))
+age2=int(input("entrer l'age de persone 2 : "))
+if age1>age2:
+    print ("the persone 1 is older")
+elif age2>age1:
+    print("the persone 2 is older")   
+else:
+    print("they have same age ")     
