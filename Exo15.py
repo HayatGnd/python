f56781918ae85a57b5fc5c9e63570f046b33b978
HayatGnd <hayatguendoul98@gmail.com>
2025-01-15 19:15:52 +0100

    exo

---
+++ Exo15.py
@@ -0,0 +1,10 @@
+mot=input("Input string in lowercase")
+#pourque string lowercase officialy
+mot=mot.lower()
+print("Le voyale 'a' dans le mot ? :")
+print("a" in mot)
+print("Le voyale 'e' dans le mot ? :")
+print("e" in mot)
+print("Le voyale 'o' dans le mot ? :")
+print("o" in mot)
+
