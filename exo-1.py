a345b048470c4dc98ec60fece6803a3d850a971d
HayatGnd <hayatguendoul98@gmail.com>
2025-01-15 22:13:50 +0100

    add exo 1

---
+++ exo-1.py
@@ -0,0 +1,9 @@
+name=input("input your name : ")
+if name=="VIP":
+   print("Enjoy the show it's for free!!")
+else:
+     nb=int(input("How many tickets you wanna buy ? :"))
+     price=nb*15.50
+     print(f"the total cost is : {price}")
+     print("Enjoy the tickets ! ")   
+    
