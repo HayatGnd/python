a10a0433cea821a491c9442eaebc676aa6769530
HayatGnd <hayatguendoul98@gmail.com>
2025-01-15 17:25:53 +0100

    add new file

---
+++ exo.py
@@ -0,0 +1,40 @@
+number1 = 2.5
+number2 = -1.25
+number3 = 3.62
+x = 11e3
+y = 13E4
+z = -87.7e10
+cond=5<=6
+print(cond)
+if cond:
+    print(cond )
+moyenne = (number1 + number2 + number3) / 3
+print(f"mean:{moyenne}")
+numberabs=int(input("enter number :"))
+if numberabs<0:
+    numberabs=numberabs*-1
+print(f"the number is:{numberabs}")                                                                                                                                                        
+
+
+
+
+print("""This is the best programming course,
+I ever have in my life,
+I really enjoy Python""")
+print("This is the best programming course,")
+age=int(input("entrer votre age:"))
+if age<=18:
+    print("hello Mineur")
+    print("next customer")
+
+number=int(input("enterer un num :"))
+if number<0:
+    print("nombre negative")
+if number>0:
+    print("numbre positive")
+if number==0:
+    print("number nulle")        
+    
+    
+
+
